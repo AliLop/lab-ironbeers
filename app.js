a614f35996e421c60ec362443a48b27323ed2af9
AliLop”git config --global user.email alicia.hl87@gmail.comgit config --global user.name AliLop” <65405578+AliLop@users.noreply.github.com>
2020-11-03 17:49:34 +0000

    done

--- app.js
+++ app.js
@@ -13,13 +13,32 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(__dirname + '/views/partials');
 
 // Add the route handlers here:
-
 app.get('/', (req, res) => {
   res.render('index');
+});
+app.get('/beers', (req, res) => {
+
+  punkAPI.getBeers()  // instance da nova classe
+    .then((response) => {
+      console.log(response);
+      res.render('beers', {beers: response});
+    })
+    .catch((error) => {
+      console.log('error');
+    })
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI.getRandom()
+  .then(randomResponse => {
+    // your magic happens here
+    res.render('random-beers', randomResponse[0] );
+    console.log(randomResponse);
+  })
+  .catch(error => console.log(error)); 
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
